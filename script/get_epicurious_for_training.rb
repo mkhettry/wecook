9175a8c6c0b10cbd431ea395844e447a1e8d09f9
mkhettry <manish.khettry@gmail.com>
2011-04-11 23:18:32 -0700

    Add 100 files from epicurious.

--- script/get_epicurious_for_training.rb
+++ script/get_epicurious_for_training.rb
@@ -11,13 +11,13 @@ doc = Nokogiri::HTML(open(ARGV[0]))
 links=doc.xpath('//a[contains(@href, "recipes/food")]')
 urls=links.collect {|l| "http://epicurious.com/" + l.attribute('href').to_s}.uniq
 urls.each do |url|
-  next unless url =~ /\/\d+/
+#  next unless url =~ /\/\d+/
 
   rd = RecipeDocument.new(:url => url)
   ingredients = rd.extract_ingredients_structured
   prep = rd.extract_prep_structured
 
-  name = "config/training/" +  url.split("/")[2].gsub(".", "_") + "_" + url.hash.abs.to_s + ".trn"
+  name = "config/training/" +  url.split("/")[2].gsub(".", "_") + "_" + url.hash.abs.to_s + ".trs"
   next if (File.exists?(name))
 
   f = File.open(name, 'w')
