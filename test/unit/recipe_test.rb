5c38e2b16ae5bdc405833ed2d279cd06237f4f2d
mkhettry <manish.khettry@gmail.com>
2011-05-24 08:51:44 -0700

    Add ordinal to ingredient. Fix test for provisional path

--- test/unit/recipe_test.rb
+++ test/unit/recipe_test.rb
@@ -11,6 +11,22 @@ class RecipeTest < ActiveSupport::TestCase
   end
 
   test "correct provisional recipe" do
+    recipe = create_provisional_recipe ["IN\ta", "OT\tb", "PR\tc", "OT\td"]
+    recipe.correct! 1 => "IN"
+    assert_equal 2, recipe.ingredients.length
+    assert_equal 0, recipe.ingredients[0].ordinal
+    assert_equal 1, recipe.ingredients[1].ordinal
 
+  end
+
+  private
+  def create_provisional_recipe(lines)
+    composite_line = ""
+    lines.each do |line|
+      composite_line += line
+      composite_line += "\n"
+
+    end
+    Recipe.new(:page =>composite_line)
   end
 end
