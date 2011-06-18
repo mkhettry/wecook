0356c346df2e3c3a06846213b216376227f730e9
skondaji <kr.srinidhi@gmail.com>
2011-06-18 13:32:18 -0700

    native user creation bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(params[:user])
+    @user.provider = "native"
     if @user.save
       session[:user_id] = @user.id
       redirect_to recipes_path
