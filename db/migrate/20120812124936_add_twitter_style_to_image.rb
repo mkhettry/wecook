95e504cf2a6205eabcc97e7f5d270d2bb34543fd
mkhettry <manish.khettry@gmail.com>
2012-08-12 06:49:50 -0700

    try up/down style of migration

--- db/migrate/20120812124936_add_twitter_style_to_image.rb
+++ db/migrate/20120812124936_add_twitter_style_to_image.rb
@@ -1,5 +1,9 @@
 class AddTwitterStyleToImage < ActiveRecord::Migration
-  def change
+  def self.up
     add_column :images, :twitter_style, :boolean
+  end
+
+  def self.down
+    remove_column :images, :twitter_style
   end
 end
