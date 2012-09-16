8af276c38266ee9d49c99d965ef041b95de269b1
kchinmay <kchinmay@someserver.com>
2012-09-15 18:31:57 -0700

    * sigin in email

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -9,7 +9,7 @@ class SessionsController < ApplicationController
     end
 
     if not user
-      redirect_to :action => "new", :loginfail => 'true'
+      redirect_to :action => "new", :loginfail => 'true', :email=> params[:email]
     else
       session[:user_id] = user.id
       redirect_to recipes_path
